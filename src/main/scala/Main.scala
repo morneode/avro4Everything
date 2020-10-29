f8448dcef4f5c71c4747de34cf01318ef1499b94
Morne Odendaal <morne@jemstep.com>
2020-10-29 09:07:13 +0200

    update

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,34 @@
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
+
+import com.sksamuel.avro4s.{AvroInputStream, AvroOutputStream, AvroSchema, Record, RecordFormat}
+
+
+
+
+
 object Main extends App {
   println("Hello, World!")
+  case class Composer(name: String, birthplace: String, compositions: Seq[String])
+
+  val ennio: Composer = Composer("ennio morricone", "rome", Seq("legend of 1900", "ecstasy of gold"))
+  val format: RecordFormat[Composer] = RecordFormat[Composer]
+  // record is of type GenericRecord
+  val record: Record = format.to(ennio)
+
+  // is an instance of Composer
+  val ennioNew = format.from(record)
+
+  val baos = new ByteArrayOutputStream()
+  val output = AvroOutputStream.binary[Composer].to(baos).build(AvroSchema[Composer])
+  output.write(ennio)
+  output.close()
+  val bytes = baos.toByteArray
+
+  val in = new ByteArrayInputStream(bytes)
+  val input = AvroInputStream.binary[Composer].from(in).build(AvroSchema[Composer])
+  val result = input.iterator.toSeq
+
+  println(s"the result is ${result}")
+//  result shouldBe Vector(ennio)
+
 }
